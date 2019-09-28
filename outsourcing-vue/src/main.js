771dbcb61715e5cddb46b8bc6accd749cf0de6b7
zxcshai <54606692+zxcshai@users.noreply.github.com>
2019-09-28 19:10:01 +0800

    完成Login

--- outsourcing-vue/src/main.js
+++ outsourcing-vue/src/main.js
@@ -1,10 +1,16 @@
+import ElementUI from 'element-ui' //新添加
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import axios from '../node_modules/axios'
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+Vue.use(ElementUI)   //新添加
 
 new Vue({
+  el: '#app',
   router,
+  components: { App },
+  template: '<App/>',
   render: h => h(App)
 }).$mount('#app')

8bae503122911f7b456e509ffd436e98f45f0226
thtTNT <848815969@qq.com>
2019-11-28 10:39:40 +0800

    添加功能: 中标

--- platformservlet/src/main/java/com/model/Bid.java
+++ platformservlet/src/main/java/com/model/Bid.java
@@ -3,20 +3,33 @@ package com.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
+import com.type.BidState;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter 
+@Getter
 @Setter
 @Entity
 @Table(name = "bid")
 public class Bid {
-  @Id
-  private Integer id;
-  private Integer projectId;
-  private Integer studioId;
-  private Integer state;
-  private Integer companyId;
-  private Integer quote;
+    @Id
+    private Integer id;
+    private Integer projectId;
+    private Integer studioId;
+    //0 等待 1 未中标 2 中标
+    private Integer state;
+    private Integer companyId;
+    private Integer quote;
+
+    @Transient
+    public BidState getState() {
+        return BidState.fromId(state);
+    }
+
+    @Transient
+    public void setState(BidState state) {
+        this.state = state.getId();
+    }
 }
--- platformservlet/src/main/java/com/service/BidService.java
+++ platformservlet/src/main/java/com/service/BidService.java
@@ -1,11 +1,15 @@
 package com.service;
 
 import com.dao.BidRepository;
+import com.dao.ProjectRepository;
 import com.model.Bid;
 import com.model.Project;
+import com.type.BidState;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BidService {
@@ -13,13 +17,36 @@ public class BidService {
     @Resource
     private BidRepository bidRepository;
 
+    @Resource
+    private ProjectRepository projectRepository;
+
     public void bid(Project project, int studioId, int quote) {
         Bid bid = new Bid();
         bid.setCompanyId(project.getCompanyID());
         bid.setProjectId(project.getId());
-        bid.setState(0);
+        bid.setState(BidState.WAIT);
         bid.setQuote(quote);
         bid.setStudioId(studioId);
         bidRepository.save(bid);
+    }
+
+    public void pick(Project project, int studioId) {
+        Optional<Bid> opBid = bidRepository.getByProjectIdAndStudioId(project.getId(), studioId);
+        if (!opBid.isPresent()) {
+            return;
+        }
+        Bid bid = opBid.get();
+        bid.setState(BidState.WIN);
+        bidRepository.save(bid);
+        List<Bid> bids = bidRepository.findAllByProjectId(project.getId());
+        bids.forEach(it -> {
+            if (it.getStudioId() != studioId) {
+                it.setState(BidState.REJECT);
+            }
+        });
+        bidRepository.saveAll(bids);
+
+        project.setStudioID(studioId);
+        projectRepository.save(project);
     }
 }

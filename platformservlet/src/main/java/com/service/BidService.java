b082a7607724ec78d8ea2c1632d03898fe1abeaf
thtTNT <848815969@qq.com>
2019-11-28 10:12:43 +0800

    添加功能: 投标

--- platformservlet/src/main/java/com/service/BidService.java
+++ platformservlet/src/main/java/com/service/BidService.java
@@ -1,8 +1,25 @@
 package com.service;
 
+import com.dao.BidRepository;
+import com.model.Bid;
+import com.model.Project;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class BidService {
 
+    @Resource
+    private BidRepository bidRepository;
+
+    public void bid(Project project, int studioId, int quote) {
+        Bid bid = new Bid();
+        bid.setCompanyId(project.getCompanyID());
+        bid.setProjectId(project.getId());
+        bid.setState(0);
+        bid.setQuote(quote);
+        bid.setStudioId(studioId);
+        bidRepository.save(bid);
+    }
 }

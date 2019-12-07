3c3f2533998a8a25a6c0f30fae3bafec7cc51d76
HaotianTu <Haotiantu@126.com>
2019-12-08 02:12:59 +0800

    修复BUG: 忘传了个文件

--- platformservlet/src/main/java/com/utils/alipay/PayeeInfo.java
+++ platformservlet/src/main/java/com/utils/alipay/PayeeInfo.java
@@ -1,4 +1,27 @@
 package com.utils.alipay;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class PayeeInfo {
+
+    @JSONField(name = "identity")
+    private String identity;
+
+    @JSONField(name = "identity_type")
+    private String identityType = "ALIPAY_USER_ID";
+
+    public void setAccount(String account) {
+        identityType = "ALIPAY_USER_ID";
+        identity = account;
+    }
+
+    public void setUID(String uid) {
+        identityType = "ALIPAY_LOGON_ID";
+        identity = uid;
+    }
+
 }

f794d997637739a9ea73c998fd09df25e2567a71
thtTNT <848815969@qq.com>
2019-12-07 21:36:00 +0800

    代码重构

--- platformservlet/src/main/java/com/dp/dto/ServerAdvertisement.java
+++ platformservlet/src/main/java/com/dp/dto/ServerAdvertisement.java
@@ -1,4 +1,27 @@
 package com.dp.dto;
 
-public class ServeraAdvertisement {
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class ServerAdvertisement {
+
+    private int id;
+    private String belong;
+    private int type;
+    private int money;
+    @JsonProperty("solr_id")
+    private String solrId;
+    private int state;
+
+    public ServerAdvertisement(int id, String belong, int type, int money, String solrId, int state) {
+        this.id = id;
+        this.belong = belong;
+        this.type = type;
+        this.money = money;
+        this.solrId = solrId;
+        this.state = state;
+    }
 }
--- platformservlet/src/main/java/com/dp/dto/ServerVerification.java
+++ platformservlet/src/main/java/com/dp/dto/ServerVerification.java
@@ -1,4 +1,23 @@
 package com.dp.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ServerVerification {
+
+    private int id;
+    private int type;
+    private int state;
+    private String username;
+    private String img;
+    private int userId;
+
+    public ServerVerification(int id, int type, int state, String username, String img, int userId) {
+        this.id = id;
+        this.type = type;
+        this.state = state;
+        this.username = username;
+        this.img = img;
+        this.userId = userId;
+    }
 }
